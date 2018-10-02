517cfa2a96496c5ada0548be1f615cc78a4047c6
Valentin Charbonnier <valentinbreiz@gmail.com>
2018-10-02 16:15:31 +0200

    strlen added.

--- source/lib/stdio.c
+++ source/lib/stdio.c
@@ -2,15 +2,26 @@
 
 int printf(const char *string, ...)
 {
-	asm volatile ("mov   $0xC, %%edx\n"
-				 "mov   %0, %%ecx\n"
-				 "mov   $0x1, %%ebx\n"
-				 "mov   $0x4, %%eax\n"
-				 "int   $0x48\n"
-				 "mov   $0x1, %%eax\n"
-				 "int   $0x48\n"
-				 :
-				 : "r"(string)
-				 : "edx", "ebx", "eax" , "ecx", "memory");
+	int len = strlen(string);
+	asm volatile (
+				"mov   %1, %%edx\n"
+				"mov   %0, %%ecx\n"
+				"mov   $0x1, %%ebx\n"
+				"mov   $0x4, %%eax\n"
+				"int   $0x48\n"
+				"mov   $0x1, %%eax\n"
+				"int   $0x48\n"
+				: "=r"(len)
+				: "r"(string)
+				: "edx", "ebx", "eax" , "ecx", "memory"
+				);
 	return 0;
+}
+
+int strlen(const char *str)
+{
+    int retval;
+    for (retval = 0; *str != '\0'; str++)
+        retval++;
+    return retval;
 }
